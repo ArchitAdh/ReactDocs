15759285b8e5ec12c35e4754809fa94d6c042192
ArchitAdh. <architadhikary@gmail.com>
2024-12-02 17:36:29 +0545

    map function refresher" ;

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
-const user = {
-  name: "Archili mercedes",
-  imageUrl:
-    "https://images.unsplash.com/photo-1528837224361-a7f556d646b9?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  imageSize: 90,
-};
+const products = [
+  { title: "phone", id: 1 },
+  { title: "laptop", id: 2 },
+  { title: "tablet", id: 3 },
+];
 
 function MyApp() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={`photo of ${user.name}`}
-        style={{ width: user.imageSize, height: user.imageSize }}
-      />
-    </>
-  );
+  const listItems = products.map((product) => (
+    <li key={product.id}> {product.title}</li>
+  ));
+  return <ul>{listItems}</ul>;
 }
 
 const container = document.getElementById("root");
